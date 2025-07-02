7daa961db07f639504a7984e30ffbcf67fddb1f9
Nguyễn Duy Lâm <126323960+notDuyLam@users.noreply.github.com>
2025-07-02 18:13:39 +0700

    test push docker image lab

--- spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/ApiGatewayApplication.java
+++ spring-petclinic-api-gateway/src/main/java/org/springframework/samples/petclinic/api/ApiGatewayApplication.java
@@ -38,6 +38,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 
 import java.time.Duration;
 
+// small change to test push docker image lab 2
 
 /**
  * @author Maciej Szarlinski
